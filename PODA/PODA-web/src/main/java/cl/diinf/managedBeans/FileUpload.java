e705de27c5641ed30d72f7f95c8fcf1b44c9fb6a
José Ignacio Cortés Tapia <jose.cortes@usach.cl>
2015-04-30 17:47:07 -0300

    Integración de subida del archivo y HOME, sin validaciones

--- PODA/PODA-web/src/main/java/cl/diinf/managedBeans/FileUpload.java
+++ PODA/PODA-web/src/main/java/cl/diinf/managedBeans/FileUpload.java
@@ -5,28 +5,79 @@
  */
 package cl.diinf.managedBeans;
 
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import cl.diinf.sessionBeans.OA_Reader;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.Part;
 
 /**
  *
  * @author teban
  */
 @Named(value = "fileUpload")
-@Dependent
+@RequestScoped
 public class FileUpload {
+   
+    private Part file;
+    private String fileContent;
+    private boolean fileUp;
+ 
+    public void upload() {
+        try {
+          fileContent = new Scanner(file.getInputStream())
+              .useDelimiter("\\A").next();
+        } catch (IOException e) {
+          // Error handling
+        }
+    }
 
-    /**
-     * Creates a new instance of FileUpload
-     */
-    public FileUpload() {
-        OA_Reader nuevo = new OA_Reader();
+    public Part getFile() {
+      return file;
+    }
 
+    public void setFile(Part file) {
+      this.file = file;
+      
+    }
+
+    public String getFileContent() {
+        return fileContent;
+    }
+
+    public void setFileContent(String fileContent) {
+        this.fileContent = fileContent;        
+    }
+
+    public boolean isFileUp() {
+        return fileUp;
+    }
+
+    public void setFileUp(boolean fileUp) {
+        this.fileUp = fileUp;
     }
     
-    
-    
-    
-    
+    public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+        
+        List<FacesMessage> msgs = new ArrayList<FacesMessage>();
+        
+        Part file = (Part)value;
+        if (file.getSize() > 1024) {
+          msgs.add(new FacesMessage("file too big"));
+        }
+        if (!"text/plain".equals(file.getContentType())) {
+          msgs.add(new FacesMessage("not a text file"));
+        }
+        if (!msgs.isEmpty()) {
+          throw new ValidatorException(msgs);
+        }
+    }            
 }
